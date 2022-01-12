e9774593712422f36c235daa0490520137325c03
james-abuh <michealjames12311@gmail.com>
2022-01-12 02:42:57 -0800

    First commit

---
+++ src/components/data.js
@@ -0,0 +1,159 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Google Pixel - Black",
+        img: "img/images3.jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "samsung",
+        img: "./img/images4.jpg",
+        price: 20,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Nokia",
+        img: "img/index.jpg",
+        price: 30,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Infinix",
+        img: "img/images2.jpg",
+        price: 40,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "Teckno",
+        img: "img/images3.jpg",
+        price: 50,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Itel",
+        img: "img/index.jpg",
+        price: 60,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Blackberry",
+        img: "img/images4.jpg",
+        price: 80,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "iphone",
+        img: "img/images3.jpg",
+        price: 90,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0,
+    },
+    {
+        id: 9,
+        title: "Motorola",
+        img: "img/images2.jpg",
+        price: 1000,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 10,
+        title: "Hawauei",
+        img: "img/images.jpg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 11,
+        title: "Itel",
+        img: "img/index.jpg",
+        price: 60,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 12,
+        title: "Teckno",
+        img: "img/images3.jpg",
+        price: 50,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    }     
+];
+export const detailProduct =
+    {
+        id: 1,
+        title: "Pixel - Black",
+        img: "img/index.jpg",
+        price: 50,
+        company: "GOOGLE",
+        info: 
+        "lorem ggh jjgg  hhjjvb fgnnyh g gbbhhg jjjjki kikikiu jjhhjkjh kkjjk",
+        inCart: false,
+        count: 0,
+        total: 0
+    };
